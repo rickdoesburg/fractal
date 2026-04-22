796bff0db66f430307c4ab1bed0f3b2da0e56d2a
Rick Doesburg 🍕 <rdoesburg@ilionx.com>
2026-04-22 08:29:08 +0200

    Minor updates

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,7 +5,7 @@ module.exports = {
         'prettier/prettier': true,
         'no-descending-specificity': null,
         'font-family-no-missing-generic-family-keyword': null,
-        'scss/at-import-partial-extension': null,
+        'property-no-deprecated': null, // Allow deprecated clip property for screen readers
         'selector-class-pattern': null, // Allow BEM-style class names
         'scss/at-mixin-pattern': null, // Allow camelCase mixin names
         'scss/operator-no-unspaced': null, // Allow compact math expressions
